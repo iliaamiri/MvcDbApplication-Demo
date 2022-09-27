2e2a533b6700ad74481096d296a7f3e816aed403
Ilia Abedian <eilia.abedian@yahoo.com>
2022-09-27 12:12:13 -0700

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcDbApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
